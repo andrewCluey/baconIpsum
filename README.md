7b76b59348d4fda2f6e5be26869361f40d21766e
Andrew Clure <andrew@asc-solutions.co.uk>
2019-10-30 12:39:16 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # baconIpsum
 Generate some meat and ipsum fillers
+
+## Get the meat!
+Simply add this script to your powershell profile and you can generate lots of meat and ipsum fller whenever you like using a simple PowerShell command.
+
+For example:
+
+```powershell
+get-meat -Paras 2 -Type all-meat -MakeItSpicy
+```
+
+will return something like this:
+
+```text
+Spicy jalapeno in ad turducken veniam nisi culpa pig.  Spare ribs do ipsum kielbasa short ribs ut ex beef ribs shoulder sunt.  Ham tempor cow doner drumstick ipsum lorem quis laboris boudin tail est.  Eiusmod salami pig, ut magna voluptate lorem qui ball tip veniam.
+
+Boudin salami leberkas adipisicing dolore quis in picanha jowl turkey t-bone dolor.  Ham deserunt frankfurter burgdoggen brisket, beef officia cillum pariatur jowl pork chop shankle.  Beef ribs proident kevin, sunt ut tempor enim.  Swine spare ribs tongue eiusmod ribeye.  Bacon occaecat ball tip excepteur ground round.
+```
+
+## Future enhancements
+Feature enhancements currently being developed include:
+- Generate html page with random pictures of meat sculptures
+- An 'email me' or 'Slack me' option to have the meat sent directly to your inbox/channel
---
+++ get-meat.ps1
@@ -0,0 +1,66 @@
+
+function get-meat
+<#
+.SYNOPSIS
+    Gets bacon ipsum
+.DESCRIPTION
+
+.PARAMETER type
+    Required parameter. The type of content to return. Can be either "all-meat" (think all you can BBQ Rib buffet, with a side of Donner).
+    Or, can be meat-and-filler. Where ipsum filler is included.
+
+.PARAMETER paras
+    Specifes the number of paragraphs to return. Default is 1.
+
+.PARAMETER MakeItSpicy
+    Have a spicy entree with your meat.
+
+.EXAMPLE
+    Get-Meat -paras 3 -type all-meat
+    This example will return a mass of meat. Donner, Ribs, Salami. Pretty much everything you can think of (thankfully no tripe)
+
+    .EXAMPLE
+    Get-Meat -Paras 2 -Type meat-and-filler -MakeItSpicy
+    This will return 2 paragraphs of meat with ipsum filler, with a spicy entree.
+
+.NOTES
+    Author: Andrew Clure
+    Date:   October 29th 2019
+#>
+{
+# Define parameters 
+param(
+    [Parameter(Mandatory=$True)]
+    [ValidateSet("all-meat", "meat-and-filler")]
+    [String]$Type,
+    [Parameter(Mandatory=$false)]
+    [switch]$makeItSpicy,
+    [Parameter(Mandatory=$false)]
+    [int]$Paras = 1
+    )
+
+# Main fucntion block
+Process
+    {
+        Try
+        {    # Request a Spicy jalapeno starter
+            if ($MakeItSpicy) {
+                $url = "https://baconipsum.com/api/?type=$type&paras=$paras&make-it-spicy=1&format=text"
+                $data = Invoke-WebRequest $url
+                write-host $data
+            }
+            # If no starter, then just return meat and filler
+            else {
+                $url = "https://baconipsum.com/api/?type=$type&paras=$paras&format=text"
+                $data = Invoke-WebRequest $url
+                write-host $data
+            }         
+        }
+    
+        Catch
+        {
+            write-warning “Error digesting all that meat”  
+        }
+    }
+}
+
